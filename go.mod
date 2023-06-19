206367c1b7e3fb0271fd039e8d336f40b4bd86f4
sonunutanix <sonukumar.singh@nutanix.com>
2023-06-19 13:37:50 +0530

    init go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sonunutanix/tech_talk
+
+go 1.18
+
+require github.com/gofrs/uuid v4.4.0+incompatible
---
+++ main.go
@@ -0,0 +1,11 @@
+package example
+
+import (
+	"fmt"
+
+	_ "github.com/gofrs/uuid"
+)
+
+func TechTalk() {
+	fmt.Println("tech-talk-assignment-pakacge")
+}
